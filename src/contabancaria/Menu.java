d4689cc1c859e2de6cfe3ea2d9de127499fdd259
Guilherme A. Silva <guilherme.salmeida@mercadolivre.com>
2022-11-07 11:56:41 -0300

    Aula da manhã (07/11/22) - Adissão de interface Repository, keyPress e alteração no Menu

--- src/contabancaria/Menu.java
+++ src/contabancaria/Menu.java
@@ -5,6 +5,7 @@ import contabancaria.model.ContaCorrente;
 import contabancaria.model.ContaPoupanca;
 import contabancaria.util.Cores;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Menu {
@@ -20,11 +21,11 @@ public class Menu {
         conta1.visualizar();
         conta1.depositar(5000.0f);
 
-        ContaPoupanca contaPoupanca1 = new ContaPoupanca(2,123,2,"Rafael",100000.0f,15);
+        ContaPoupanca contaPoupanca1 = new ContaPoupanca(2, 123, 2, "Rafael", 100000.0f, 15);
         contaPoupanca1.visualizar();
 
 
-       /* while (true) {
+        while (true) {
             System.out.println(Cores.TEXT_YELLOW + Cores.ANSI_BLACK_BACKGROUND + "****************************************************************");
             System.out.println("*                                                              *");
             System.out.println("*                       BANCO DO BRAZIL                        *");
@@ -32,12 +33,14 @@ public class Menu {
             System.out.println("****************************************************************");
             System.out.println("*                                                              *");
             System.out.println("*          1 - Criar Conta                                     *");
-            System.out.println("*          2 - Ver Saldo da Conta                              *");
-            System.out.println("*          3 - Dados da Conta                                  *");
-            System.out.println("*          4 - Sacar                                           *");
-            System.out.println("*          5 - Depositar                                       *");
-            System.out.println("*          6 - Transferir                                      *");
-            System.out.println("*          7 - Sair                                            *");
+            System.out.println("*          2 - Listar todas as contas                          *");
+            System.out.println("*          3 - Buscar Conta por Numero                         *");
+            System.out.println("*          4 - Atualizar Dados da Conta                        *");
+            System.out.println("*          5 - Apagar Conta                                    *");
+            System.out.println("*          6 - Sacar                                           *");
+            System.out.println("*          7 - Depositar                                       *");
+            System.out.println("*          8 - Transferir Valores Entre Contas                 *");
+            System.out.println("*          9 - Sair                                            *");
             System.out.println("*                                                              *");
             System.out.println("****************************************************************");
             System.out.print("Entre com a opção desejada: ");
@@ -53,33 +56,52 @@ public class Menu {
             switch (opcao) {
                 case 1:
                     System.out.println("\nCriar Conta");
-
+                    keyPress();
                     break;
                 case 2:
-                    System.out.println("\nVer Saldo");
-
+                    System.out.println("\nBuscar Todas as Contas");
+                    keyPress();
                     break;
                 case 3:
-                    System.out.println("\nDados da Conta");
-
+                    System.out.println("\nBuscar Conta por Numero");
+                    keyPress();
                     break;
                 case 4:
-                    System.out.println("\nSacar");
-
+                    System.out.println("\nAtualizar Dados da Conta");
+                    keyPress();
                     break;
                 case 5:
-                    System.out.println("\nDepistar");
-
+                    System.out.println("\nApagar Conta");
+                    keyPress();
                     break;
                 case 6:
-                    System.out.println("\nTransferir");
-
+                    System.out.println("\nSacar");
+                    keyPress();
+                    break;
+                case 7:
+                    System.out.println("\nDepositar");
+                    keyPress();
+                    break;
+                case 8:
+                    System.out.println("\nTransferir Valores Entre Contas");
+                    keyPress();
                     break;
 
                 default:
                     System.out.println("OPÇÃO INVÁLIDA!!");
+                    keyPress();
+                    break;
             }
 
-        }*/
+        }
+    }
+
+    public static void keyPress() {
+        try {
+            System.out.println(Cores.TEXT_RESET + "\n\nPressione Enter para continuar...");
+            System.in.read();
+        } catch (IOException e) {
+            System.out.println("Você pressionou uma tecla diferente de enter!");
+        }
     }
 }
